6572421886fcbab47a4532e1e5dfc435f5e539cd
przemol <pstempor@gmail.com>
2017-07-31 20:40:35 +0100

    convert entrezid to gene names

--- count_reads.R
+++ count_reads.R
@@ -2,6 +2,7 @@ require(GenomicAlignments)
 require(Rsamtools)
 require(SummarizedExperiment)
 require(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(org.Hs.eg.db)
 
 
 gene_model <- exonsBy(keepStandardChromosomes(TxDb.Hsapiens.UCSC.hg38.knownGene), 'gene')
@@ -12,6 +13,11 @@ bam_files <- BamFileList(dir(pattern = 'bam$'))
 seqinfo(bam_files)
 
 SE <- summarizeOverlaps(gene_model, bam_files)
-
 str(assay(SE))
+
+
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+gene_smbl <- mapIds(org.Hs.eg.db, rownames(SE), "SYMBOL", "ENTREZID")
+gene_names <- mapIds(org.Hs.eg.db, rownames(SE), "GENENAME", "ENTREZID")
+
 
